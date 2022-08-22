1a33dadfa44107ccfdb098893bc3fd034321366b
AdonyasG <adonyas2000@gmail.com>
2022-08-22 10:34:22 -0400

    code fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,13 +9,28 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *temp = list;
+	listint_t *temp;
+	listint_t *temp2;
 
-	while (temp != NULL)
+	if (list == NULL)
+		return (0);
+	temp = list;
+	temp2 = list;
+
+	while (temp2->next != NULL && temp2->next->next != NULL)
 	{
-		if (temp->next == list)
-			return (1);
 		temp = temp->next;
+		temp2 = temp2->next->next;
+		if (temp == temp2)
+		{
+			temp = list;
+			while (temp != temp2)
+			{
+				temp = temp->next;
+				temp2 = temp->next;
+			}
+			return (1);
+		}
 	}
 	return (0);
 }
