302c3f4eca7a6557d885b66759a280f84bab4fe4
AdonyasG <adonyas2000@gmail.com>
2022-08-23 14:13:59 -0400

    print fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-#   for char in range(ord('a'), ord('z')+1):
-#    print('{}'.format(chr(char)), end='')
-#   another way to do with out using string format
 for c in range(97, 123):
     print("{}".format(chr(c)), end='')
