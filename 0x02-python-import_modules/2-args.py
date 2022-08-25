1fd48c4b29f3b344f78ef76edc1c8df121baada1
AdonyasG <adonyas2000@gmail.com>
2022-08-25 08:07:29 -0400

    code fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     if (len(sys.argv)-1 == 0):
         print(f"0 arguments.")
     else:
-        print(f"{len(sys.argv)-1} argument:")
+        print(f"{len(sys.argv)-1} arguments:")
         for i in range(len(sys.argv)-1):
             print(f"{i+1}: {sys.argv[i+1]}")
