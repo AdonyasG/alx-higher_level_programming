a9090f7f5582ed14c1c5933840f8c970f6938f99
AdonyasG <adonyas2000@gmail.com>
2022-08-28 03:57:10 -0400

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+def no_c(my_string):
+    if 'c' or 'C' in my_string:
+        i = ""
+        for c in range(len(my_string)):
+            if 'C' != my_string[c] and 'c' != my_string[c]:
+                i += my_string[c]
+    return i
