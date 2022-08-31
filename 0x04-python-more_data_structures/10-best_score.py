2c48305beed7f8fde10d9a18d5094cf5e2450f72
AdonyasG <adonyas2000@gmail.com>
2022-08-31 14:00:04 -0400

    code fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        newval = max(a_dictionary, key=lambda i: a_dictionary[i])
-#        maxval = max(newval)
+        return (max(a_dictionary, key=lambda i: a_dictionary[i]))
 #        maxval = max(a_dictionary, key=a_dictionary.get)
-        return newval
-    else:
-        return None
+#        return newval
