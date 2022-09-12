45b48d084e7938b1bf661ff65affb3b485a9098e
AdonyasG <adonyas2000@gmail.com>
2022-09-12 11:56:08 -0400

    code fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value is int:
+    if value in range(-100000, 100000):
         try:
+            print("{:d}".format(value))
             return True
         except ValueError:
             return False
