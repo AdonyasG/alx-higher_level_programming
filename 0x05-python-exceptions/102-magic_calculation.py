cf63f25719e70da3788ee0b27847e3a9c7fc0837
AdonyasG <adonyas2000@gmail.com>
2022-09-12 16:08:52 -0400

    exception modified

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,9 +3,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("too far")
+                raise Exception("Too far")
             else:
-                result += (a**b)/i
+                result += (a ** b) / i
         except Exception:
             result = b + a
             break
