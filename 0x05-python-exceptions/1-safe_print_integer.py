b57121df7c8077a5bd312367d507eb481c0ef91f
AdonyasG <adonyas2000@gmail.com>
2022-09-12 12:23:07 -0400

    code fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,9 @@ def safe_print_integer(value):
             print("{:d}".format(value))
             return True
         except ValueError:
-            return False
-        return value
+            try:
+                float(value)
+                print("{:d}".format(value))
+                return True
+            except ValueError:
+                return False
