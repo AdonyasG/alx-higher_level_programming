6800c8ecbe04d340b6d0d43e09b925845c35de95
AdonyasG <adonyas2000@gmail.com>
2022-09-14 08:55:49 -0400

    Error fixed

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,9 +13,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        """area"""
-        return self.__radius ** 2 * math.pi
+        """calc area"""
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """circumference"""
+        """calc circumference"""
         return 2 * math.pi * self.__radius
