9cb54f53386a670e0bbbb8bce34a472dd6dd573a
AdonyasG <adonyas2000@gmail.com>
2022-09-28 07:06:05 -0400

    initial commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 9-student
+Contains class Student
+"""
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):        """
+        Returns dictionary description with simple data structure
+        """
+        self.__dict
