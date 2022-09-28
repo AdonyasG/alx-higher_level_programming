aad0b7eeeb0d452348585b7a3b6bc25944e4d76e
AdonyasG <adonyas2000@gmail.com>
2022-09-28 17:51:36 -0400

    code fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Module - 12-pascal triangle
+Module 12-Pascal Triangle
 """
 
 
 def pascal_triangle(n):
-    """ Returns list in pascal triangle"""
+    """Returns list in pascal triangle"""
     listt = []
     for i in range(n):
         listt.append([])
         for j in range(i + 1):
-            if j == 0 or j == 1:
+            if j == 0 or j == i:
                 listt[i].append(1)
             else:
                 listt[i].append(listt[i - 1][j - 1] + listt[i - 1][j])
-        return listt
+    return listt
