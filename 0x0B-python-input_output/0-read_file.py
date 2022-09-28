e5cb402d2680d840dc9801d31fa200ee9457fa0a
AdonyasG <adonyas2000@gmail.com>
2022-09-28 06:35:58 -0400

    mode fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ Contains function that reads and prints contents from file
 def read_file(filename=""):
     """Read and print text from file"""
     with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
