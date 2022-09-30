6b4bdc81193be48958ccdb549c40bd94e0215927
AdonyasG <adonyas2000@gmail.com>
2022-09-29 21:16:25 -0400

    initial commit

---
+++ 0x0A-python-inheritance/tests/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+"""
+
+
+class Rectangle():
+    """class Rectangle that inherits from base geometery"""
+    def __init__(self, width, height):
+        """initializtion and validation"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
