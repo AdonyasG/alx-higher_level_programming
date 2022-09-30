e590f692de646a91e57cc622d9f1d9934d640c4e
AdonyasG <adonyas2000@gmail.com>
2022-09-29 21:47:44 -0400

    code fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,10 +3,11 @@
 Module 8-rectangle
 """
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle():
+class Rectangle(BaseGeometry):
     """class Rectangle that inherits from base geometery"""
     def __init__(self, width, height):
         """initializtion and validation"""
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,7 +6,7 @@ Module 9-rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle():
+class Rectangle(BaseGeometry):
     """class Rectangle that inherits from base geometery"""
     def __init__(self, width, height):
         """initializtion and validation"""
