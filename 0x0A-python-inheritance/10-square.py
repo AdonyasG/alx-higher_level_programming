1fc08a3106e05c6e44786928a5adf3357f42ed92
AdonyasG <adonyas2000@gmail.com>
2022-09-29 21:56:05 -0400

    initial commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 10-square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """create square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
