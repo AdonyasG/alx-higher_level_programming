657922fe4b18086f0ea88ca2fe52df8bebf55ac5
AdonyasG <adonyas2000@gmail.com>
2022-09-29 21:57:51 -0400

    class documentation

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,6 +8,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class square"""
     def __init__(self, size):
         """create square"""
         self.integer_validator("size", size)
