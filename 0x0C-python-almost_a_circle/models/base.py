131beb509dacc38817a662fe4b13f8eb8056a39f
AdonyasG <adonyas2000@gmail.com>
2022-10-03 15:54:17 -0400

    save fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ import json
 import os
 
 
-class Base:
+class Base():
     """class base"""
 
     __nb_objects = 0
@@ -32,13 +32,12 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """that writes the JSON string representation of list_objs to a file"""
-        i = []
-        if list_objs is not None:
-            for j in list_objs:
-                i.append(cls.to_dictionary(j))
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as f:
-            f.write(cls.to_json_string(i))
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                f.write(cls.to_json_string([o.to_dictionary()
+                        for o in list_objs]))
 
     @staticmethod
     def from_json_string(json_string):
@@ -51,13 +50,12 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Square":
-                dummy = cls(1)
-            if cls.__name__ == "Rectangle":
-                dummmy = cls(1, 1)
-            dummy.update(**dictionary)
-            return dummy
+        if (cls.__name__ == "Square"):
+            dum = cls(4)
+        if (cls.__name__ == "Rectangle"):
+            dum = cls(4, 4)
+        dum.update(**dictionary)
+        return dum
 
     @classmethod
     def load_from_file(cls):
