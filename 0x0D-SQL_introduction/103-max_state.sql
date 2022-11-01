a8d07b849791e6010ecc307fc6e8702a3dd3624c
AdonyasG <adonyas2000@gmail.com>
2022-11-01 18:40:07 -0400

    type error fix

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- max temp of state.
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
 ~
