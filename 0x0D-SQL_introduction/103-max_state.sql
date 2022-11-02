2d6f09eb5f21f2f0f6bebcc1495cf0bccdb47ffa
AdonyasG <adonyas2000@gmail.com>
2022-11-02 04:19:54 -0400

    advanced task

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- displays max tmp
-SELECT state, AVG(value) AS max_temp FROM temperatures GROUP BY state ORDER BY max_temp;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY max_temp;
