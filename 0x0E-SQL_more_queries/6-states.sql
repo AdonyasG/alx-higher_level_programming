69f22f7d95395fe66037bd915658e6f5bee61c6b
AdonyasG <adonyas2000@gmail.com>
2022-11-02 05:46:40 -0400

    setting primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);	
