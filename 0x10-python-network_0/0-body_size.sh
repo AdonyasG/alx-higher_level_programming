6ab6ea03a8bacbf778016fe231c31e3d1c2fa7ec
AdonyasG <adonyas2000@gmail.com>
2022-12-20 02:45:23 -0500

    shebang fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # takes in URL and sends request to that URL and displays size of the body of the response
 
-curl -Is $1 | grep 'Content-Length:' | cut -f2 -d' '
+curl -Is "$1" | grep 'Content-Length:' | cut -f2 -d' '
