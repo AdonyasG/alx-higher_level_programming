ffaa332cc593e1c756f4e00a981610f17cfd5c01
AdonyasG <adonyas2000@gmail.com>
2022-12-20 02:43:46 -0500

    header fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # takes in URL and sends request to that URL and displays size of the body of the response
 
-curl -Is $1 | grep 'Content-Length' | cut -f2 -d' '
+curl -Is $1 | grep 'Content-Length:' | cut -f2 -d' '
