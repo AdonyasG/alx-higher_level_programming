8891ba9dd6b4a459e3d85d006b206d6a317ac520
AdonyasG <adonyas2000@gmail.com>
2022-12-31 16:13:05 -0500

    whitespace fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const quantity = parseInt(process.argv[2], 10);
 if (isNaN(quantity)) {
-	console.log('Not a number');
+  console.log('Not a number');
 }else {
-	console.log(`My number: ${quantity}`);
+  console.log(`My number: ${quantity}`);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 const myVar = ['C is fun','Python is cool','JavaScript is amazing'];
 let i = 0;
 while (i < myVar.length) {
-	console.log(myVar[i]);
-	i++;
+  console.log(myVar[i]);
+  i++;
 }
