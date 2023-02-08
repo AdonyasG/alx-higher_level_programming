3b0192071cc9b20ca1a24d1a4d92c40204bc520d
AdonyasG <adonyas2000@gmail.com>
2023-02-08 13:31:59 -0500

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,11 +2,11 @@
 // 8-eserver.js
 
 exports.esrever = function (list) {
-	const listnew = [];
+  const listnew = [];
 
-    for (i = list.length - 1; i >=0; i--) {
-	    const index = list[i]
-	    listnew.push(index)
-    }
-    return(listnew);
-}
+  for (let i = list.length - 1; i >= 0; i--) {
+    const index = list[i];
+    listnew.push(index);
+  }
+  return (listnew);
+};
