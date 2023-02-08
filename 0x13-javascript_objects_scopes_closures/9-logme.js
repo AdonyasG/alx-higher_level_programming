6e9b55b222e48d9efa950ecc77bea61b853634a7
AdonyasG <adonyas2000@gmail.com>
2023-02-08 14:35:35 -0500

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a function that prints the number of arguments already printed and the new argument value.
+
+let count = -1;
+exports.logMe = function (item) {
+    console.log(count + ': ' + item);
+    count++;
+};
